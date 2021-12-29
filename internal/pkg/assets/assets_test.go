19a719aad902ac4ae5ee61c0e2dcfef84d76e456
SoapiestWaffles <soapy@soapiestwaffles.com>
2021-12-29 03:06:03 -0800

    add assets unit test

---
+++ internal/pkg/assets/assets_test.go
@@ -0,0 +1,37 @@
+package assets
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestInit(t *testing.T) {
+	tests := []struct {
+		name      string
+		assetFile string
+		assetVar  *string
+	}{
+		{
+			name:      "logo",
+			assetFile: "logo.txt",
+			assetVar:  &Logo,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			content, err := ioutil.ReadFile(tt.assetFile)
+			if err != nil {
+				t.Errorf("Asset file: %s - error reading asset file: %v", tt.assetFile, err)
+			}
+			t.Logf("Asset file: %s - successfully read asset file", tt.assetFile)
+			if *tt.assetVar != string(content) {
+				t.Errorf("Asset file: file contents of %s does not match asset var", tt.assetFile)
+			}
+		})
+	}
+}
+
+func countWords(phrase string) int {
+	return len(strings.Fields(strings.TrimSpace(phrase)))
+}
