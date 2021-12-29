24587bd3653ade38526a2e947ac194b309c20710
SoapiestWaffles <soapy@soapiestwaffles.com>
2021-12-29 00:13:21 -0800

    rearrangement of directories

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/alecthomas/kong"
+	"github.com/soapiestwaffles/s3-nuke/internal/pkg/assets"
 )
 
 var cli struct {
@@ -13,6 +16,8 @@ func main() {
 	ctx := kong.Parse(&cli,
 		kong.Name("s3-nuke"),
 		kong.Description("Quickly destroy all objects and versions in an AWS S3 bucket."))
+
+	fmt.Println(assets.Logo)
 
 	var err error
 	ctx.FatalIfErrorf(err)
