628f1e71035bd73869fbc86449e2d380f45b4efd
SoapiestWaffles <soapy@soapiestwaffles.com>
2021-12-30 07:12:36 -0800

    Add bucket selection UI
    skip-checks: true

--- go.mod
+++ go.mod
@@ -18,13 +18,20 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sso v1.7.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.12.0 // indirect
 	github.com/aws/smithy-go v1.9.0 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 )
 
 require (
 	github.com/aws/aws-sdk-go v1.42.25
 	github.com/aws/aws-sdk-go-v2/config v1.11.1
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.22.0
+	github.com/briandowns/spinner v1.18.0
+	github.com/manifoldco/promptui v0.9.0
 	github.com/nwtgck/go-fakelish v0.1.3
 	github.com/pkg/errors v0.9.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
+
+	"time"
 
 	"github.com/alecthomas/kong"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/briandowns/spinner"
+	"github.com/manifoldco/promptui"
 	"github.com/soapiestwaffles/s3-nuke/internal/pkg/assets"
+	"github.com/soapiestwaffles/s3-nuke/pkg/aws"
 )
 
 const releaseURL = "https://github.com/soapiestwaffles/s3-nuke/release"
@@ -29,6 +39,8 @@ func main() {
 		kong.Description("Quickly destroy all objects and versions in an AWS S3 bucket."))
 
 	fmt.Println(assets.Logo)
+
+	//  Show version information and exit
 	if cli.Version {
 		fmt.Println("Find releases at", releaseURL)
 		fmt.Println("")
@@ -38,6 +50,75 @@ func main() {
 		os.Exit(0)
 	}
 
-	var err error
+	// Initalize AWS S3 Client
+	cfg, err := config.LoadDefaultConfig(context.TODO())
 	ctx.FatalIfErrorf(err)
+	s3Client := s3.NewFromConfig(cfg)
+
+	// Init S3 Service
+	s3 := aws.NewS3Service(s3Client)
+	if s3 == nil {
+		ctx.FatalIfErrorf(errors.New("error initializing s3 service"))
+	}
+
+	spinnerGetBuckets := spinner.New(spinner.CharSets[13], 100*time.Millisecond)
+	spinnerGetBuckets.Suffix = " fetching bucket list..."
+	spinnerGetBuckets.Color("blue", "bold")
+	spinnerGetBuckets.Start()
+	buckets, err := s3.GetAllBuckets(context.TODO())
+	spinnerGetBuckets.Stop()
+
+	selectBucketsPrompt(buckets)
+
+	ctx.FatalIfErrorf(err)
+
+}
+
+func selectBucketsPrompt(buckets []aws.Bucket) {
+	// This is a nasty hack just to dereference the `Name` field.
+	// TODO investigate more to see if we can dereference right in the template OR find a different UI library
+	derefBucket := []struct {
+		Name         string
+		CreationDate *time.Time
+	}{}
+	for _, b := range buckets {
+		derefBucket = append(derefBucket, struct {
+			Name         string
+			CreationDate *time.Time
+		}{*b.Name, b.CreationDate})
+	}
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ \"---\" | faint }} {{ . | blue | bold }} {{ \"---\" | faint }}",
+		Active:   "\U0001FAA3  {{ .Name | cyan }}",
+		Inactive: "   {{ .Name | cyan }}",
+		Selected: "\U0001FAA3  {{ .Name | bold | green }}",
+		Details: `
+------ S3 Bucket Info ------
+{{ "Name............:" | faint }} {{ .Name }}
+{{ "Creation Date...:" | faint }} {{ .CreationDate }}`,
+	}
+
+	searcher := func(input string, index int) bool {
+		bucket := derefBucket[index]
+		name := strings.Replace(strings.ToLower(bucket.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Buckets to Nuke",
+		Items:     derefBucket,
+		Templates: templates,
+		Size:      5,
+		Searcher:  searcher,
+	}
+
+	_, _, err := prompt.Run()
+
+	if err != nil {
+		return
+	}
+
 }
