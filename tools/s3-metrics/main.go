2823a20d0f424a6b0bd58fe901fc19f44ad90fa4
SoapiestWaffles <soapy@soapiestwaffles.com>
2022-02-02 08:00:12 -0800

    s3-metrics: default region, version, and debug
    This commit fixes #9 . Region is no longer required and `--region` option has been removed.
    `--version` and `--debug` options have been added

--- tools/s3-metrics/main.go
+++ tools/s3-metrics/main.go
@@ -10,15 +10,24 @@ import (
 	"github.com/briandowns/spinner"
 	"github.com/dustin/go-humanize"
 	"github.com/guptarohit/asciigraph"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/soapiestwaffles/s3-nuke/internal/pkg/ui/tui"
 	"github.com/soapiestwaffles/s3-nuke/pkg/aws/cloudwatch"
 	"github.com/soapiestwaffles/s3-nuke/pkg/aws/s3"
 )
 
+const releaseURL = "https://github.com/soapiestwaffles/s3-nuke/releases"
+
 var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+
 	cli struct {
+		Version     bool   `help:"display version information" optional:""`
 		AWSEndpoint string `help:"override AWS endpoint address" short:"e" optional:"" env:"AWS_ENDPOINT"`
-		Region      string `help:"specify region to create bucket and objects in" short:"r" default:"us-west-2" env:"AWS_REGION"`
+		Debug       bool   `help:"enable debugging output" optional:""`
 	}
 )
 
@@ -27,17 +36,36 @@ func main() {
 		kong.Name("s3-metrics"),
 		kong.Description("s3-metrics tool: get bucket object metrics for a particular bucket"))
 
+	//  Show version information and exit
+	if cli.Version {
+		fmt.Println("version....:", version)
+		fmt.Println("commit.....:", commit)
+		fmt.Println("date.......:", date)
+		fmt.Println("")
+		fmt.Println("Find new releases at", releaseURL)
+		fmt.Println("")
+		os.Exit(0)
+	}
+
+	if _, regionEnv := os.LookupEnv("AWS_REGION"); !regionEnv {
+		os.Setenv("AWS_REGION", "us-east-1")
+	}
+
 	if cli.AWSEndpoint != "" {
 		fmt.Println("Using AWS endpoint:", cli.AWSEndpoint)
 	}
 
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, NoColor: false})
+	if cli.Debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		log.Info().Msg("debug logging output enabled")
+	} else {
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	}
+
 	// Set up S3 client
 	var s3svc s3.Service
-	if cli.Region == "" {
-		s3svc = s3.NewService(s3.WithAWSEndpoint(cli.AWSEndpoint), s3.WithRegion(cli.Region))
-	} else {
-		s3svc = s3.NewService(s3.WithAWSEndpoint(cli.AWSEndpoint))
-	}
+	s3svc = s3.NewService(s3.WithAWSEndpoint(cli.AWSEndpoint))
 
 	// Get list of buckets
 	loadingSpinner := spinner.New(spinner.CharSets[13], 100*time.Millisecond)
